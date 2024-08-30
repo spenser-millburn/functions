9249f36e12a1007a6eadaa0956703b0e9c6e196c
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-08-30 13:44:15 -0400

    Add current directory tracking and improve log viewing prompt

--- bplantest.fish
+++ bplantest.fish
@@ -1,12 +1,13 @@
 function bplantest
+ set cwd $PWD
  set test (cat /home/smillburn/embd/repo/alphabot/app/planner/bot/actor/test/test.cpp | grep TEST_F | getfield 3 | fzf)
  set executable GTEST-bot_planner_actor_test
  set fixture BotPlannerActorTestFixture
  set command "gtest x86_64 $executable \*$fixture.$test\*"
  fish -c $command
  echo $command 
- prompt please press ENTER to view the log
+ echo (read --prompt-str="Press Enter to View the log")
  cdbuild alphabot-x86_64-assert/test/unit
- thl $executable.key.thl $executable.thl | lnav
  echo "thl $executable.key.thl $executable.thl | lnav"
+ cd $cwd
 end
