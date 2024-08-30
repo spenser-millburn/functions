4f4bf62f044f1b5c537645dd3e2fdc762f7aded5
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-08-30 13:38:27 -0400

    Refactor bplantest function and add logging with lnav.

--- bplantest.fish
+++ bplantest.fish
@@ -1,6 +1,12 @@
 function bplantest
  set test (cat /home/smillburn/embd/repo/alphabot/app/planner/bot/actor/test/test.cpp | grep TEST_F | getfield 3 | fzf)
- set command "gtest x86_64 GTEST-bot_planner_actor_test \*BotPlannerActorTestFixture.$test\*"
+ set executable GTEST-bot_planner_actor_test
+ set fixture BotPlannerActorTestFixture
+ set command "gtest x86_64 $executable \*$fixture.$test\*"
  fish -c $command
  echo $command 
+ prompt please press ENTER to view the log
+ cdbuild alphabot-x86_64-assert/test/unit
+ thl $executable.key.thl $executable.thl | lnav
+ echo "thl $executable.key.thl $executable.thl | lnav"
 end
