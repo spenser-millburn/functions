5f097c17cff51e1e7e41455b988405f1109a00fd
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-09-30 11:16:47 -0400

    Refactor askadoc and askthedocs, add fuzzaskadoc function.

--- askthedocs.fish
+++ askthedocs.fish
@@ -1,10 +1,12 @@
 function askthedocs
-    if test -e askthedocs.txt
+    if test -e /tmp/askthedocs.txt
         gptfask /tmp/askthedocs.txt "$argv[2..-1]"
     else
-        set links (python3 -m askthedocs.askthedocs --url $argv[1] --print-links-only --maxdepth 3 --filter-out \#)
+        set links (python3 -m askthedocs.askthedocs --url $argv[1] --print-links-only --maxdepth 2 --filter-out \#)
+        echo $links 
+        prompt press enter to continue
         for link in $links
-            curl $link | html2text >> askthedocs.txt
+            curl $link | html2text >> /tmp/askthedocs.txt
         end
         gptfask /tmp/askthedocs.txt "$argv[2..-1]"
     end
--- fuzzaskadoc.fish
+++ fuzzaskadoc.fish
@@ -1,4 +1,4 @@
-function askadoc
+function fuzzaskadoc
  set links (python3 -m askthedocs.askthedocs --url "$argv[1]" --print-links-only --maxdepth 2 --filter-out \#)
 echo $links | g from this list please select the link most related to "$argv[2]" you may only return one link | xargs curl | html2text | g "$argv[3..-1]"
 end
