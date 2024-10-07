23869a2c0ab93bd7a67cfd3c280687e7731cf6ad
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-07 10:37:34 -0400

    Updated gcm function to include message type in commit.

--- gcm.fish
+++ gcm.fish
@@ -1,11 +1,12 @@
 function gcm
     set diff (git diff)
     set stat (git status)
+    set message (g "diff : $diff , status: $stat. please write a 5 word commit message")
+    set message_type (g "diff : $diff , status: $stat. please respond in one word if this is an ADD, REMOVE, UPDATE or FIX")
 
     if not git config --get user.email > /dev/null
         git config --global user.email "you@example.com"
         git config --global user.name "Your Name"
     end
-
-    git commit -am (g "diff : $diff , status: $stat. please write a 5 word commit message")
+    git commit -am "[$message_type] $message"
 end
