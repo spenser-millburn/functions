337bc63d80c37a56739809770ae9121f4337a34d
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-28 14:22:40 -0400

    Update scripts for Python3 and correct log file path.

--- log2csv.fish
+++ log2csv.fish
@@ -1,3 +1,3 @@
 function log2csv
-python $ALPHABOT_REPO_DIR/python/ab2pandas.py -o csv $argv
+python3 $ALPHABOT_REPO_DIR/python/ab2pandas.py -o csv $argv
 end
--- logview.fish
+++ logview.fish
@@ -1,5 +1,6 @@
 function logview
-  thl -ts utc (logdir)/$argv[1]/$argv[1].key.thl (logdir)/$argv[1]/$argv[1].thl > (logdir)/$argv[1].txt 
-  lnav $argv[1].txt
+  thl -ts utc (logdir)/$argv[1]/$argv[1].key.thl (logdir)/$argv[1]/$argv[1].thl > (logdir)/$argv[1]/$argv[1].txt  
+  lnav (logdir)/$argv[1]/$argv[1].txt
+
   # echo (logdir)/$argv[1]/ | clip
 end
