c39756b16e426d330a790a82271eb7d2e667ed05
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-27 23:57:03 -0400

    Add scripts to copy print and string repr helper headers

---
+++ create_print_helpers_h.fish
@@ -0,0 +1,3 @@
+function create_print_helpers_h
+cp /home/smillburn/embd/repo/boilerplate/cpp_helpers/print_helpers.h .
+end
---
+++ create_string_repr_helpers_h.fish
@@ -0,0 +1,3 @@
+function create_string_repr_helpers_h
+cp /home/smillburn/embd/repo/boilerplate/cpp_helpers/string_repr_helpers.h .
+end
